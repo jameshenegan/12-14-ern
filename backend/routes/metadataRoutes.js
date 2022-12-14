1c55c73af7361b8e654bd6332f660ada7de8df6c
Jim <jim@Jims-MBP.attlocal.net>
2022-12-14 06:58:37 -0600

    set up endpoint to get searchable metadata

--- backend/routes/metadataRoutes.js
+++ backend/routes/metadataRoutes.js
@@ -2,23 +2,36 @@ const express = require("express");
 
 const { loadCsvFile } = require("../helpers/loadCsvFile");
 const { getArrayOfVarDois } = require("../helpers/getArrayOfVarDois");
+const {
+  getMetadataFieldsWithVarDoi,
+} = require("../helpers/getMetadataFieldsWithVarDoi");
 
 // Config
-const varDoi = "var_doi";
+const varDoi = require("../config/varDoi.json");
+const searchableFields = require("../config/searchableFields.json");
 
 // Main program
 const router = express.Router();
 
 const metadata = loadCsvFile("backend/csv-files/variable-level-metadata.csv");
 const arrayOfVarDois = getArrayOfVarDois(metadata, varDoi);
+const searchableMetadata = getMetadataFieldsWithVarDoi(
+  searchableFields,
+  metadata,
+  varDoi
+);
 
-// Test the endpoint
+// Endpoints
 router.get("/records", (req, res) => {
   res.status(200).json(metadata);
 });
 
 router.get("/arrayOfVarDois", (req, res) => {
   res.status(200).json(arrayOfVarDois);
+});
+
+router.get("/searchableMetadata", (req, res) => {
+  res.status(200).json(searchableMetadata);
 });
 
 module.exports = router;
