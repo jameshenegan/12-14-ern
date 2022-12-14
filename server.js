d5579950c3d193248e35a1197905c54fce480cef
Jim <jim@Jims-MBP.attlocal.net>
2022-12-14 06:34:37 -0600

    set up public folder

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+// Serve static assets in production
+// Set static folder
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
