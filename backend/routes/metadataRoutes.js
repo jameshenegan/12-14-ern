c04ecc0c8ab1429c9a5fb250324981d3b30e540c
Jim <jim@Jims-MBP.attlocal.net>
2022-12-14 06:54:54 -0600

    set up endpoint to get array of var dois

--- backend/routes/metadataRoutes.js
+++ backend/routes/metadataRoutes.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const router = express.Router();
 
 const { loadCsvFile } = require("../helpers/loadCsvFile");
+const { getArrayOfVarDois } = require("../helpers/getArrayOfVarDois");
+
+// Config
+const varDoi = "var_doi";
+
+// Main program
+const router = express.Router();
 
 const metadata = loadCsvFile("backend/csv-files/variable-level-metadata.csv");
+const arrayOfVarDois = getArrayOfVarDois(metadata, varDoi);
 
 // Test the endpoint
 router.get("/records", (req, res) => {
   res.status(200).json(metadata);
+});
+
+router.get("/arrayOfVarDois", (req, res) => {
+  res.status(200).json(arrayOfVarDois);
 });
 
 module.exports = router;
